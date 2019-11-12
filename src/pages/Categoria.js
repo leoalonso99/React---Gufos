781e256f0ac6ef15d0c4c9e2c2bdcd115d54b0e5
leoalonso99 <FIC@info.local>
2019-11-12 13:37:14 -0300

    React - Começo do site de eventos GUFOS

---
+++ src/pages/Categoria.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import "../App.css";
+
+
+class Categoria extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Categoria</h1>
+                <p>teste Categoria</p>
+                <footer>Senai de informatica. 2019</footer>
+            </div>
+
+        )
+    }
+
+}
+
+export default Categoria;
